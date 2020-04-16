6074066988bdd0f2420176e825e4e728aa8f0ad5
McBKorir <34438978+ipkipkorir@users.noreply.github.com>
2020-04-16 17:25:36 +0300

    Update estimator.py
    Modified computations

--- src/estimator.py
+++ src/estimator.py
@@ -14,7 +14,7 @@ def estimator(data):
 	impactCurrentlyInfected = data['reportedCases'] * 10
 	impactInfectionsByRequestedTime = impactCurrentlyInfected * (2 ** (timeInDays // 3))
 	impactSevereCasesByRequestedTime = math.trunc((0.15 * impactInfectionsByRequestedTime) // 1)
-	impactHospitalBedsByRequestedTime = math.trunc((((0.35 * data['totalHospitalBeds']) * timeInDays) - impactSevereCasesByRequestedTime) // 1)
+	impactHospitalBedsByRequestedTime = math.trunc(((0.35 * data['totalHospitalBeds']) - impactSevereCasesByRequestedTime) // 1)
 	impactCasesForICUByRequestedTime = math.trunc((0.05 * impactInfectionsByRequestedTime) // 1)
 	impactCasesForVentilatorsByRequestedTime = math.trunc((0.02 * impactInfectionsByRequestedTime) // 1)
 	impactDollarsInFlight = math.trunc((((impactInfectionsByRequestedTime * data["region"]['avgDailyIncomePopulation']) * data["region"]['avgDailyIncomeInUSD']) // timeInDays) // 1)
@@ -24,7 +24,7 @@ def estimator(data):
 	severeImpactCurrentlyInfected = data['reportedCases'] * 50
 	severeImpactInfectionsByRequestedTime = severeImpactCurrentlyInfected * (2 ** (timeInDays // 3))
 	severeImpactSevereCasesByRequestedTime =  math.trunc((0.15 * severeImpactInfectionsByRequestedTime) // 1)
-	severeImpactHospitalBedsByRequestedTime = math.trunc((((0.35 * data['totalHospitalBeds']) * timeInDays)- severeImpactSevereCasesByRequestedTime) // 1)
+	severeImpactHospitalBedsByRequestedTime = math.trunc(((0.35 * data['totalHospitalBeds']) - severeImpactSevereCasesByRequestedTime) // 1)
 	severeImpactCasesForICUByRequestedTime = math.trunc((0.05 * severeImpactInfectionsByRequestedTime) // 1)
 	severeImpactCasesForVentilatorsByRequestedTime = math.trunc((0.02 * severeImpactInfectionsByRequestedTime) // 1)
 	severeImpactDollarsInFlight = math.trunc((((severeImpactInfectionsByRequestedTime * data["region"]['avgDailyIncomePopulation']) * data["region"]['avgDailyIncomeInUSD']) // timeInDays) // 1)
